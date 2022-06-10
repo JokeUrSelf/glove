5d533ae033f1e086902c583ab318d53df775d6c5
Mik <privacy@email.com>
2022-06-10 11:17:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GLove
+A fast code a editor with keyboard-only support Based on Flutter
---
+++ android/app/src/main/kotlin/com/example/glove/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.glove
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
